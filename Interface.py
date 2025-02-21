e867c574c743bf7178c8457c8366b538a69da48f
caamano1 <joaocaamano87@gmail.com>
2025-02-21 11:33:36 -0300

    Crianção da janela da interface

---
+++ Interface.py
@@ -0,0 +1,15 @@
+import json
+import os
+import tkinter as tk
+from tkinter import messagebox
+
+
+janela = tk.Tk()
+janela.title("Gerenciador de Tarefas")
+janela.configure(bg="white")
+
+ARQUIVO_JSON = "tarefas.json"
+
+
+
+janela.mainloop()
